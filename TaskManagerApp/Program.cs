ce57f0affa701779eb42dc14da98d327f1341d0b
Medha <medhaop23@gmail.com>
2025-11-25 20:23:02 +0530

    fix path

--- TaskManagerApp/Program.cs
+++ TaskManagerApp/Program.cs
@@ -1,46 +1,47 @@
-
 using TaskManagerApp.Data;
 using Microsoft.EntityFrameworkCore;
-
-
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Read the connection string
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+// If running on Azure (Production), use absolute SQLite path
+if (builder.Environment.IsProduction())
+{
+    var dbPath = Path.Combine("D:\\home\\site\\wwwroot", "TaskManager.db");
+    connectionString = $"Data Source={dbPath}";
+}
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(connectionString));
 
 var app = builder.Build();
 
+// Ensure database exists
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.EnsureCreated();
 }
 
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-
 app.UseAuthorization();
-
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 
 app.Run();
